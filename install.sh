1b0c182dcf45d819fd016bbb667bbcff6abea19c
Nicolas de Chevigné <nico2che@users.noreply.github.com>
2020-10-06 14:00:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [ ! -d $installdir ]; then
     exit 1
 else
     cd $installdir 
-    ansible-playbook -i ./hosts playbook.yml --verbose
+    ansible-playbook -i ./hosts playbook.yml --verbose --tags=setup,apps,desktop -e 'perso=yes pro=yes'
 fi
 
 echo "cleaning up..."
